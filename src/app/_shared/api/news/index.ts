e5ac61063cf95870b5f74a5aa12e2b7ff3c3ac5d
Zhicheng Wang <zcwang@thoughtworks.com>
2016-09-13 15:06:47 +0800

    加上一篇正式的新闻：Angular 2 Final即将到来！

--- src/app/_shared/api/news/index.ts
+++ src/app/_shared/api/news/index.ts
@@ -4,13 +4,12 @@ import {Article} from '../../models/article';
 
 const items: Article[] = [
   {
-    id: 'AngularDart独立啦',
-    image: require('./_images/split.svg'),
-    title: 'AngularDart独立啦！',
-    summary: 'Angular开发组决定分离出一个单独的Dart分支，任其自由发展',
-    content: require('./10.AngularDart独立啦.md'),
-    authors: ['Naomi Black'],
-    originalUrl: 'http://angularjs.blogspot.hk/2016/07/a-dedicated-team-for-angulardart.html'
+    id: 'Angular 2 Final即将到来！',
+    image: require('./10.Angular 2 Final即将到来/final.svg'),
+    title: 'Angular 2 Final即将到来！！',
+    summary: 'Angular 2 Final即将到来，敬请期待',
+    content: require('./10.Angular 2 Final即将到来/_index.md'),
+    authors: ['雪狼'],
   }
 ];
 
