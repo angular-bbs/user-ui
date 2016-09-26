a7ad44223570dd7b67ff83f1fa493064c82e224a
Zhicheng Wang <zcwang@thoughtworks.com>
2016-09-26 20:27:42 +0800

    增补了Angular 2发布公告

--- src/app/_shared/api/news/index.ts
+++ src/app/_shared/api/news/index.ts
@@ -10,6 +10,15 @@ const items: Article[] = [
     summary: 'Angular 2 Final即将到来，敬请期待',
     content: require('./10.Angular 2 Final即将到来/_index.md'),
     authors: ['雪狼'],
+  },
+  {
+    id: 'Angular 2正式发布',
+    image: require('../../_images/angular.svg'),
+    title: 'Angular 2正式发布',
+    summary: '2016年中秋节，经过两年半的长跑，Angular 2终于正式发布了！',
+    content: require('./20.Angular 2正式发布/_index.md'),
+    authors: ['Jules Kremer'],
+    translators: ['雪狼'],
   }
 ];
 
