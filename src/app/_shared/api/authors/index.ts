1523db4318f4a3dbff9c3e85c707d7f666f5efda
indooorsman <indooorsman@gmail.com>
2016-10-12 15:37:14 +0800

    add author 王传业

--- src/app/_shared/api/authors/index.ts
+++ src/app/_shared/api/authors/index.ts
@@ -1,6 +1,6 @@
-import {Injectable} from '@angular/core';
-import {Author} from '../../models/author';
-import {Observable} from 'rxjs/Observable';
+import {Injectable} from "@angular/core";
+import {Author} from "../../models/author";
+import {Observable} from "rxjs/Observable";
 const items: Author[] = [
   {
     id: '雪狼',
@@ -19,6 +19,15 @@ const items: Author[] = [
     avatar: require('./_images/余泽江.jpg'),
     columnist: false,
     homepage: 'https://github.com/trotyl/'
+  },
+  {
+    id: 'indooorsman',
+    name: '王传业',
+    bio: '前端老司机，驾龄7年',
+    description: require('./66.王传业.md'),
+    avatar: require('./_images/王传业.jpg'),
+    columnist: false,
+    homepage: 'https://csser.me'
   }
 ];
 @Injectable()
