b197be0d782a97aef2e6e220bac9a74088fe7623
shallops <534933174@qq.com>
2016-10-13 18:02:02 +0800

    支持vscode 调试 chrome

--- src/app/_shared/services/change-title.ts
+++ src/app/_shared/services/change-title.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Title } from '@angular/platform-browser';
+
 @Injectable()
 export class ChangeTitle implements CanActivateChild {
-  canActivateChild(childRoute: ActivatedRouteSnapshot, 
-  state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  canActivateChild(childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     let route: ActivatedRouteSnapshot = childRoute;
     let routes: ActivatedRouteSnapshot[] = [];
     while (route) {
@@ -13,7 +15,7 @@ export class ChangeTitle implements CanActivateChild {
     }
     document.title = routes
       .filter(route => route.data && (<any>route.data).title)
-      .map(route => (<any> route.data).title)
+      .map(route => (<any>route.data).title)
       .join(' / ');
     return true;
   }
