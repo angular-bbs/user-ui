9121efc932519559ed2e6659c81a8b3a5a3cb0de
shallops <534933174@qq.com>
2016-10-13 11:10:50 +0800

    异步加载module

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
-import AboutModule from './about/_about.module';
-import BbsModule from './bbs/_bbs.module';
-import LibraryModule from './library/_library.module';
-import UserModule from './user/_user.module';
+// import AboutModule from './about/_about.module';
+// import BbsModule from './bbs/_bbs.module';
+// import LibraryModule from './library/_library.module';
+// import UserModule from './user/_user.module';
 import { ChangeTitle } from './_shared/services/change-title';
 
 const routes: Routes = [
@@ -12,10 +12,22 @@ const routes: Routes = [
     canActivateChild: [ChangeTitle],
     children: [
       { path: '', pathMatch: 'full', redirectTo: '/user' },
-      { path: 'about', data: { title: '关于我们' }, loadChildren: () => AboutModule, },
-      { path: 'bbs', data: { title: '微站论坛' }, loadChildren: () => BbsModule, },
-      { path: 'library', data: { title: '资料中心' }, loadChildren: () => LibraryModule, },
-      { path: 'user', data: { title: '用户中心' }, loadChildren: () => UserModule, },
+      {
+        path: 'about', data: { title: '关于我们' },
+        loadChildren: () => System.import('./about/_about.module')
+      },
+      {
+        path: 'bbs', data: { title: '微站论坛' },
+        loadChildren: () => System.import('./bbs/_bbs.module')
+      },
+      {
+        path: 'library', data: { title: '资料中心' },
+        loadChildren: () => System.import('./library/_library.module')
+      },
+      {
+        path: 'user', data: { title: '用户中心' },
+        loadChildren: () => System.import('./user/_user.module')
+      },
       { path: '**', component: NotFoundComponent },
     ]
   },
