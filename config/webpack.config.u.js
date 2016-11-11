59d8569bb6f55d76acca43f23353027064ced06f
rxjs-space <rxjs-space@outlook.com>
2016-11-10 22:13:03 -0800

    adds u:scripts and webpack.config.u.js and modifies custom-typings.d.ts

---
+++ config/webpack.config.u.js
@@ -0,0 +1,145 @@
+var webpack = require('webpack');
+var path = require('path');
+var clone = require('js.clone');
+var webpackMerge = require('webpack-merge');
+
+// var CompressionPlugin = require("compression-webpack-plugin"); // see in giz: below
+
+var commonPlugins = [
+  new webpack.ContextReplacementPlugin(
+    // The (\\|\/) piece accounts for path separators in *nix and Windows
+    /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
+    root('./src'),
+    {
+      // your Angular Async Route paths relative to this root directory
+    }
+  ),
+
+  // gzip: 
+  // To use gzip, you can run 'npm install compression-webpack-plugin --save-dev'
+  // add 'var CompressionPlugin = require("compression-webpack-plugin");' on the top
+  // and comment out below codes
+  //
+  // new CompressionPlugin({
+  //   asset: "[path].gz[query]",
+  //   algorithm: "gzip",
+  //   test: /\.js$|\.css$|\.html$/,
+  //   threshold: 10240,
+  //   minRatio: 0.8
+  // })
+];
+
+var commonConfig = {
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  context: __dirname,
+  output: {
+    publicPath: path.resolve(__dirname),
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      // TypeScript
+      { test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] },
+      { test: /\.html$/, loader: 'raw-loader' },
+      { test: /\.css$/, loader: 'raw-loader' },
+      { test: /\.json$/, loader: 'json-loader' }
+    ],
+  },
+  plugins: [
+    // Use commonPlugins.
+  ]
+
+};
+
+// Client.
+var clientPlugins = [
+
+];
+
+var clientConfig = {
+  target: 'web',
+  entry: './src/client',
+  output: {
+    path: root('dist/client')
+  },
+  node: {
+    global: true,
+    __dirname: true,
+    __filename: true,
+    process: true,
+    Buffer: false
+  }
+};
+
+
+// Server.
+var serverPlugins = [
+
+];
+
+var serverConfig = {
+  target: 'node',
+  entry: './src/server', // use the entry file of the node server if everything is ts rather than es5
+  output: {
+    path: root('dist/server'),
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    loaders: [
+      { test: /@angular(\\|\/)material/, loader: "imports-loader?window=>global" }
+    ],
+  },
+  externals: includeClientPackages([
+    // include these client packages so we can transform their source with webpack loaders
+
+    // '@angular/common',
+    // '@angular/compiler',
+    // '@angular/core',
+    // '@angular/forms',
+    // '@angular/http',
+    // '@angular/platform-browser',
+    // '@angular/platform-browser-dynamic',
+    // '@angular/platform-server',
+    // '@angular/router',
+
+    '@angular/material'
+  ]),
+  node: {
+    global: true,
+    __dirname: true,
+    __filename: true,
+    process: true,
+    Buffer: false
+  }
+};
+
+module.exports = [
+  // Client
+  webpackMerge(clone(commonConfig), clientConfig, { plugins: clientPlugins.concat(commonPlugins) }),
+
+  // Server
+  webpackMerge(clone(commonConfig), serverConfig, { plugins: serverPlugins.concat(commonPlugins) })
+];
+
+function includeClientPackages(packages) {
+  return function(context, request, cb) {
+    if (packages && packages.indexOf(request) !== -1) {
+      return cb();
+    }
+    return checkNodeImport(context, request, cb);
+  };
+}
+// Helpers
+function checkNodeImport(context, request, cb) {
+  if (!path.isAbsolute(request) && request.charAt(0) !== '.') {
+    cb(null, 'commonjs ' + request); return;
+  }
+  cb();
+}
+
+function root(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  return path.join.apply(path, [__dirname].concat(args));
+}
