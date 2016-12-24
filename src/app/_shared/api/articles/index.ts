b4fcfe92d00bffc415d0941b3a084c2fc539ff2c
xiedapao <846432362@qq.com>
2016-12-24 17:08:41 +0800

    adjust title

--- src/app/_shared/api/articles/index.ts
+++ src/app/_shared/api/articles/index.ts
@@ -4,8 +4,8 @@ import {Observable} from 'rxjs/Observable';
 
 const items: Article[] = [
     {
-    id: '如何工程化开发大型angular2项目',
-    title: '如何工程化开发大型angular2项目',
+    id: '如何工程化开发大型angular2项目（上篇续）',
+    title: '如何工程化开发大型angular2项目（上篇续）',
     summary: 'angular2刚出不久，很多技术主管在观望是否要把angular2投入生存环境，作为一名作死者，从今年3月就投入生存环境，8个月来挖坑经验告诉你，绝对没问题。已经百家大型客户投入使用！',
     content: require('./231.如何工程化开发大型angular2项目（上篇续）/_index.md'),
     first: true,
